1c477db9f5b62b7156ce68d3c49a62d1ee256779
514K <alekseysafronov0@gmail.com>
2019-11-05 19:26:38 +0300

    debug_version

---
+++ sas.py
@@ -0,0 +1,35 @@
+P = int()
+Q = int()
+sup = int()
+N = float(0)
+seriesK = []
+
+H = float(0)
+
+P = int(input(">>>insert P:\n>>>"))
+Q = int(input(">>>insert Q:\n>>>"))
+sup = int(2 * Q - 1)            #supremum series
+for n in range(sup):            #here sum N 
+    if n == 0:
+        k = 1
+        N += k
+        seriesK.append(k)
+    if n >= 1 and n <= (Q - 1):
+        k = 1 + 2 * ((P * n)/Q)
+        N += k
+        seriesK.append(k)
+    if n == Q:
+        k = 2 * P - 1
+        N += k
+        seriesK.append(k)
+    if n >= (Q + 1) and n <= (sup):
+        k = 1 + 2 * (2 * P - (P * n)/Q)
+        N += k
+        seriesK.append(k)
+for n in range(sup):
+    for k in seriesK:
+        H += (9 / N) * ( ((1 - n / Q) ** 2) - ((k / P) ** 2) ) ** 2
+print(H)
+print(P, Q, sup, N)
+print(seriesK) 
+print(2*P*Q, N)
